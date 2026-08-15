31ad57b66151426ad6c3fd34713c05446098e6d5
reddotmediang-ui <reddotmediang@gmail.com>
2026-08-15 17:48:23 +0100

    update on featured items

--- src/components/FeaturedGallery.js
+++ src/components/FeaturedGallery.js
@@ -5,37 +5,37 @@ export default function FeaturedGallery() {
   const featuredItems = [
     {
       id: 1,
-      title: 'African Transportation',
+      title: 'Transportation',
       image: 'https://media.gettyimages.com/id/637382222/photo/tricycle-driver.jpg?s=612x612&w=0&k=20&c=djHBvonOhcE1pgyG4AcVrW_chXVI6DmZ8yOFFlg8Pcg=',
       link: '/images/transportation'
     },
     {
       id: 2,
-      title: 'African Market',
+      title: 'Market',
       image: 'https://media.gettyimages.com/id/2159863988/photo/fashion-designer-draping-cloth-on-a-mannequin.jpg?s=612x612&w=0&k=20&c=RetMrlhlXhpLCT-DO9L4JfLT03ByNn-Twrnlwh8__yk=',
       link: '/images/market'
     },
     {
       id: 3,
-      title: 'African Lifestyle',
+      title: 'Lifestyle',
       image: 'https://media.gettyimages.com/id/2149573150/photo/lady-carrying-shopping-paper-bags.jpg?s=612x612&w=0&k=20&c=XHgQhTfpnwoN0mz2BeWiRYQphHUXXKfNgGk0eaSXSrA=',
       link: '/images/lifestyle'
     },
     {
       id: 4,
-      title: 'African Nature',
+      title: 'Nature',
       image: 'https://media.gettyimages.com/id/2237820120/photo/macro-view-tiny-moss-plants.jpg?s=612x612&w=0&k=20&c=EIQcuXB-0VcT9m_8eemznlmG8T76uPl-qJ1RNL5H5SE=',
       link: '/images/nature'
     },
     {
       id: 5,
-      title: 'African Aerial',
+      title: 'Aerial',
       image: 'https://media.gettyimages.com/id/2253043338/photo/aerial-view-of-lagos-island.jpg?s=612x612&w=0&k=20&c=ZLnJCSFhB-ZUb4bOco5iO41QTbUu_5jCEG0PA8x1iBo=',
       link: '/images/aerial'
     },
     {
       id: 6,
-      title: 'African Food',
+      title: 'Food',
       image: 'https://images.pond5.com/garden-egg-and-grinded-groundnut-photo-249174816_iconl_nowm.jpeg',
       link: '/images/food'
     }
